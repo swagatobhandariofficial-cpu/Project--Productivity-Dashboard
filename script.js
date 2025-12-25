4e9e7f0e3c520d02e0d7b6aec961b46ed6908a1b
Swagato Bhanadari <swagatobhandariofficial@gmail.com>
2025-12-25 21:06:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+class kitaaab {
+    constructor(name,price,author,color){
+        this.name=name;
+        this.price=price;
+        this.author=author;
+        this.color=color;
+    }
+}
+
+let k1 = new kitaaab("moral",600,"debanjan","orange");
+let k2 = new kitaaab("Indian Constitution",362,"Swatantra Sananies","dark blue");
+let k3 = new kitaaab("Rich dad poor dad",350,"Unknown","blue");
