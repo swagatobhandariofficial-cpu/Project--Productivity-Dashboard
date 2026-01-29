98e35399d0120454870870fb9ce4825bd9ba3233
Swagato Bhanadari <swagatobhandariofficial@gmail.com>
2026-01-29 19:12:11 +0530

    callback1

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-function getUser(user, cb){
+// function getUser(user, cb){
+//     setTimeout(() => {
+//         cb({id: 1, username:"_spriiha." })
+//     }, 1000);
+// }
+
+// function getUserPosts(id, cb){
+//     setTimeout(() => {
+//         cb(["Hola", "Amigos" , "Good day"])
+//     },2000)
+// }
+
+// getUser("Swagato", function(details){
+//     getUserPosts(details.id, function(allposts){
+//         console.log(details.username, allposts)
+//     } )
+// })
+
+function loginUser(username, cb){
+    console.log("logging in user....")
     setTimeout(() => {
-        cb({id: 1, username:"_spriiha." })
+        cb({user:"Swagato Bhandari"})
     }, 1000);
 }
 
-function getUserPosts(id, cb){
+function fetchPermissions(userId, cb) {
+    console.log("fetching permissions....")
     setTimeout(() => {
-        cb(["Hola", "Amigos" , "Good day"])
-    },2000)
+        cb(["read", "send message", "follow", "like"])
+    }, 2000);
 }
 
-getUser("Swagato", function(details){
-    getUserPosts(details.id, function(allposts){
-        console.log(details.username, allposts)
-    } )
+function loadDashboard(permissions, cb) {
+    console.log("loading dashboard....")
+    setTimeout(() => {
+        cb();
+    }, 1000);
+}
+
+
+loginUser("_spriiha.", function (user) {
+    fetchPermissions(user.id, function (permissions) {
+        loadDashboard(permissions, function () {
+            console.log("✅Dashboard Loaded.")
+        })
+    })
 })
