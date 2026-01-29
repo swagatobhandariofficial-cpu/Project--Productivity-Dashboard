708815425171bb6b3fb97092795f1e5f99991561
Swagato Bhanadari <swagatobhandariofficial@gmail.com>
2026-01-29 20:13:35 +0530

    fetching from api

--- script.js
+++ script.js
@@ -1,51 +1,10 @@
-//----------------call Back example 1
-
-function getUser(user, cb){
-    setTimeout(() => {
-        cb({id: 1, username:"_spriiha." })
-    }, 1000);
-}
-
-function getUserPosts(id, cb){
-    setTimeout(() => {
-        cb(["Hola", "Amigos" , "Good day"])
-    },2000)
-}
-
-getUser("Swagato", function(details){
-    getUserPosts(details.id, function(allposts){
-        console.log(details.username, allposts)
-    } )
+fetch('https://randomuser.me/api/')
+.then(function(readdetails){
+    return readdetails.json();
 })
-
-//-----------------------call back example 2
-
-function loginUser(username, cb){
-    console.log("logging in user....")
-    setTimeout(() => {
-        cb({user:"Swagato Bhandari"})
-    }, 1000);
-}
-
-function fetchPermissions(userId, cb) {
-    console.log("fetching permissions....")
-    setTimeout(() => {
-        cb(["read", "send message", "follow", "like"])
-    }, 2000);
-}
-
-function loadDashboard(permissions, cb) {
-    console.log("loading dashboard....")
-    setTimeout(() => {
-        cb();
-    }, 1000) ;
-}
-
-
-loginUser("_spriiha.", function (user) {
-    fetchPermissions(user.id, function (permissions) {
-        loadDashboard(permissions, function () {
-            console.log("✅Dashboard Loaded.")
-        })
-    })
+.then(function(realdata){
+    console.log(realdata.results[0].name.first);
 })
+.catch(function (err) {
+    console.log(err)
+});
