d735d9278a354d4b07858df1287f39f2e056c6be
Swagato Bhanadari <swagatobhandariofficial@gmail.com>
2026-01-30 18:34:02 +0530

    promises and Async Await

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-fetch('https://randomuser.me/api/')
-.then(function(readdetails){
-    return readdetails.json();
-})
-.then(function(realdata){
-    console.log(realdata.results[0].name.first);
-})
-.catch(function (err) {
-    console.log(err)
-});
+//---------------fetching weather report using promises method(then-catch)
+function abcd(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=93bc8c534dced6b5c372972a29ae7236`)
+        .then(function (raw) {
+            return raw.json();
+        })
+        .then(function (real) {
+            console.log(real);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+abcd("london");
+
+
+//--------------fetching weather report using async-await
+    function xyz(city) {
+    setTimeout(async() => {
+        let rawData= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=93bc8c534dced6b5c372972a29ae7236`)
+        let realData= await rawData.json()
+        console.log(realData)
+    }, 3000);
+};
+xyz("Delhi");
