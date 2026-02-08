abd3415477754779aff4d709b189f2c62ae99d89
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-02-08 12:29:41 +0530

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,72 @@
-function debounce(fn,delay) {
-    let timer;
-    return function(){
-        clearTimeout(timer)
-        timer=setTimeout(fn,delay)
-    }
+function openFeatures() {
+    var allElems=document.querySelectorAll('.elem');
+    var fullElemPage =document.querySelectorAll('.fullElem')
+    var fullElemPageBackbtn =document.querySelectorAll('.fullElem .back')
+
+    allElems.forEach(function (elem) {
+        elem.addEventListener('click',function () {
+            fullElemPage[elem.id].style.display='block'
+        })
+    })
+
+    fullElemPageBackbtn.forEach(function(back){
+        back.addEventListener('click',function(){
+            fullElemPage[back.id].style.display='none'
+        })
+    })
+}
+openFeatures()
+
+
+
+
+
+var currentTask=[]
+if(localStorage.getItem('currentTask')){
+    currentTask = JSON.parse(localStorage.getItem('currentTask'))
+}else{
+    console.log('Task list is empty');
 }
 
-document.querySelector("#search").addEventListener("input", debounce (function() {
-    console.log("chalgaya")
-},500))
+
+
+function renderTask(){
+    var allTask = document.querySelector('.allTask')
+
+    var sum=''
+
+    currentTask.forEach(function(elem){
+        sum=sum+ `<div class="task">
+                            <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+                            <button>Mark as Completed</button>
+                        </div>`
+        console.log(elem)
+    })
+    allTask.innerHTML=sum;
+}
+renderTask();
+
+
+let form=document.querySelector('.addTask form');
+let taskInput= document.querySelector('.addTask form #task-input')
+let taskDetailsInput= document.querySelector('.addTask form textarea')
+let taskCheckbox=document.querySelector('.addTask form #check');
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    currentTask.push(
+        {
+            task:taskInput.value ,
+            details:taskDetailsInput.value ,
+            imp:taskCheckbox.checked
+        })
+        localStorage.setItem('currentTask',JSON.stringify(currentTask))
+        taskInput.value=''
+        taskDetailsInput.value=''
+        taskCheckbox.checked=false
+    renderTask();
+})
+
+var markCompletedBtn = document.querySelectorAll('.task button')
+
