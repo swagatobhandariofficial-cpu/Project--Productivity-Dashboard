c265790f641ec34d058372b35e0d8f0d10328309
Swagato Bhandari <swagatobhandariofficial@gmail.com>
2026-02-09 09:51:16 +0530

    second commit

--- script.js
+++ script.js
@@ -1,72 +1,87 @@
 function openFeatures() {
-    var allElems=document.querySelectorAll('.elem');
-    var fullElemPage =document.querySelectorAll('.fullElem')
-    var fullElemPageBackbtn =document.querySelectorAll('.fullElem .back')
+    var allElems = document.querySelectorAll('.elem')
+    var fullElemPage = document.querySelectorAll('.fullElem')
+    var fullElemPageBackBtn = document.querySelectorAll('.fullElem .back')
 
     allElems.forEach(function (elem) {
-        elem.addEventListener('click',function () {
-            fullElemPage[elem.id].style.display='block'
+        elem.addEventListener('click', function () {
+            fullElemPage[elem.id].style.display = 'block'
         })
     })
 
-    fullElemPageBackbtn.forEach(function(back){
-        back.addEventListener('click',function(){
-            fullElemPage[back.id].style.display='none'
+    fullElemPageBackBtn.forEach(function (back) {
+        back.addEventListener('click', function () {
+            fullElemPage[back.id].style.display = 'none'
         })
     })
 }
+
 openFeatures()
 
 
+function todoList() {
+
+    var currentTask = []
+
+    if (localStorage.getItem('currentTask')) {
+        currentTask = JSON.parse(localStorage.getItem('currentTask'))
+    } else {
+        console.log('Task list is Empty');
+    }
 
 
 
-var currentTask=[]
-if(localStorage.getItem('currentTask')){
-    currentTask = JSON.parse(localStorage.getItem('currentTask'))
-}else{
-    console.log('Task list is empty');
-}
+    function renderTask() {
 
+        var allTask = document.querySelector('.allTask')
 
+        var sum = ''
 
-function renderTask(){
-    var allTask = document.querySelector('.allTask')
-
-    var sum=''
-
-    currentTask.forEach(function(elem){
-        sum=sum+ `<div class="task">
-                            <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
-                            <button>Mark as Completed</button>
-                        </div>`
-        console.log(elem)
-    })
-    allTask.innerHTML=sum;
-}
-renderTask();
-
-
-let form=document.querySelector('.addTask form');
-let taskInput= document.querySelector('.addTask form #task-input')
-let taskDetailsInput= document.querySelector('.addTask form textarea')
-let taskCheckbox=document.querySelector('.addTask form #check');
-
-form.addEventListener('submit',function(e){
-    e.preventDefault()
-
-    currentTask.push(
-        {
-            task:taskInput.value ,
-            details:taskDetailsInput.value ,
-            imp:taskCheckbox.checked
+        currentTask.forEach(function (elem, idx) {
+            sum = sum + `<div class="task">
+        <h5>${elem.task} <span class="${elem.imp ? 'true' : 'false'}">imp</span></h5>
+        <button id=${idx}>Mark as Completed</button>
+        </div>`
         })
-        localStorage.setItem('currentTask',JSON.stringify(currentTask))
-        taskInput.value=''
-        taskDetailsInput.value=''
-        taskCheckbox.checked=false
-    renderTask();
-})
 
-var markCompletedBtn = document.querySelectorAll('.task button')
+        allTask.innerHTML = sum
 
+        localStorage.setItem('currentTask', JSON.stringify(currentTask))
+
+        document.querySelectorAll('.task button').forEach(function (btn) {
+            btn.addEventListener('click', function () {
+                currentTask.splice(btn.id, 1)
+                renderTask()
+            })
+        })
+    }
+    renderTask()
+
+    let form = document.querySelector('.addTask form')
+    let taskInput = document.querySelector('.addTask form #task-input')
+    let taskDetailsInput = document.querySelector('.addTask form textarea')
+    let taskCheckbox = document.querySelector('.addTask form #check')
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault()
+        currentTask.push(
+            {
+                task: taskInput.value,
+                details: taskDetailsInput.value,
+                imp: taskCheckbox.checked
+            }
+        )
+        renderTask()
+
+        taskCheckbox.checked = false
+        taskInput.value = ''
+        taskDetailsInput.value = ''
+    })
+
+
+
+}
+
+todoList()
+
+localStorage.clear()
